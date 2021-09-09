3077724c73e6970718e242a9f9426dbcd67864a2
misaruiz <misaruiz@gmail.com>
2021-09-09 16:54:42 -0700

    Got the modal to work

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ![This is an illustration of Misael Ruiz](https://github.com/misaruiz/04-oop-game-show-app/blob/main/images/misa-avatar.png?raw=true)
 
-## [My Public API Requests](#) :point_left
+## [My Public API Requests](https://misaruiz.github.io/05-public-api-requests/) :point_left:
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
-
 const gallery = document.getElementById('gallery');
 
-
-
 /**
- * Fetch Fucntions
+ * Fetch Functions
  */
 
 function fetchData(url) {
@@ -19,18 +16,26 @@ Promise.all([
     fetchData('https://randomuser.me/api/?results=12')
 ])
     .then(data => {
-        console.log(data[0].results[1].picture.large)
+        // console.log(data[0].results)
         const employeeList = data[0].results;
         for (let i=0; i < Object.keys(employeeList).length; i++) {
             generateGallery(employeeList[i])
         }
-        
+        return data;
+    })
+    .then(data => {
+        // console.log(data[0]);
+        const employeeCards = gallery.querySelectorAll('.card');
+        const employee = data[0].results;
+        for (let i=0; i<employeeCards.length; i++) {
+            employeeCards[i].addEventListener('click', e => {
+                generateModal(employee[i])
+            })
+        }
     })
 
 
-
-    // users.then(user => {for (let i=0; i<user.results.length; i++) { console.log(user.results[i].email) }})
-
+    
 
 /**
  * Helper Functions
@@ -42,9 +47,6 @@ function generateGallery(employee) {
     const email = employee.email;
     const city = employee.location.city;
     const state = employee.location.state;
-    const address = `${employee.location.street.number} ${employee.location.street.name}, ${city}, ${state}, ${employee.location.postcode}`;
-    const phone = employee.phone;
-    const birthday = employee.dob.date;
 
     const employeeCard = `
         <div class="card">
@@ -62,3 +64,46 @@ function generateGallery(employee) {
     gallery.insertAdjacentHTML('beforeend', employeeCard)
 
 }
+
+
+function generateModal(employee) {
+    const avatar = employee.picture.large;
+    const name = `${employee.name.first} ${employee.name.last}`;
+    const email = employee.email;
+    const city = employee.location.city;
+    const state = employee.location.state;
+    const address = `${employee.location.street.number} ${employee.location.street.name}, ${city}, ${state}, ${employee.location.postcode}`;
+    const phone = employee.phone;
+    const birthday = employee.dob.date;
+
+    const employeeModal = `
+        <div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${avatar}" alt="profile picture">
+                    <h3 id="${name}" class="modal-name cap">${name}</h3>
+                    <p class="modal-text">${email}</p>
+                    <p class="modal-text cap">${city}</p>
+                    <hr>
+                    <p class="modal-text">(${phone}</p>
+                    <p class="modal-text">${address}</p>
+                    <p class="modal-text">Birthday: ${birthday}</p>
+            </div>
+        </div>
+    `;
+
+    document.body.insertAdjacentHTML('beforeend', employeeModal)
+
+}
+
+/**
+ * Modal
+ */
+
+// for (let i=0; i<employeeCards.length; i++) {
+//     employeeCards[i].addEventListener('click', e => {
+//         generateModal(e.target)
+//     })
+// }
+
