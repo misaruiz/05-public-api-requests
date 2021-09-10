420f432b9cd70b6eff4cf2a07a5bfd99728e79cb
misaruiz <misaruiz@gmail.com>
2021-09-09 22:27:32 -0700

    Just missing the Prev/Next functionality

--- js/scripts.js
+++ js/scripts.js
@@ -20,24 +20,16 @@ Promise.all([
         return data;
     })
     .then(data => {
-        // console.log(data[0]);
-        const employeeCards = gallery.querySelectorAll('.card');
-        const employee = data[0].results;
-        for (let i=0; i<employeeCards.length; i++) {
-            employeeCards[i].addEventListener('click', e => {
-                generateModal(employee[i])
-            })
-        }
+        listenForModal(data)
+        return data;
     })
-
-
-    
+    .then(data => {
+        searchSubmit(data)
+    })
 
 /**
  * Helper Functions
  */
-
-
 function generateCard(employee) {
     const avatar = employee.picture.large;
     const name = `${employee.name.first} ${employee.name.last}`;
@@ -57,18 +49,15 @@ function generateCard(employee) {
             </div>
         </div>
     `;
-
     gallery.insertAdjacentHTML('beforeend', employeeCard)
 }
 
 function generateGallery(data) {
     const employeeList = data[0].results;
         for (let i=0; i < Object.keys(employeeList).length; i++) {
-            generateCard(employeeList[i])
+            generateCard(employeeList[i]);
         }
-
 }
-
 
 function generateModal(employee) {
     const avatar = employee.picture.large;
@@ -99,19 +88,104 @@ function generateModal(employee) {
                     <p class="modal-text">Birthday: ${birthday}</p>
             </div>
         </div>
+            <div class="modal-btn-container">
+                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                <button type="button" id="modal-next" class="modal-next btn">Next</button>
+            </div>
+        </div>
     `;
+
 
     document.body.insertAdjacentHTML('beforeend', employeeModal);
 
     const closeButton = document.getElementById('modal-close-btn');
+    const modalContainer = document.querySelector('.modal-container');
     closeButton.addEventListener('click', (e) => {
-        document.querySelector('.modal-container').remove();
+        modalContainer.remove();
     })
+}
+
+function listenForModal(data) {
+    const employeeCards = gallery.querySelectorAll('.card');
+        const employee = data[0].results;
+        for (let i=0; i<employeeCards.length; i++) {
+            employeeCards[i].addEventListener('click', e => {
+                // const currentEmployee = employee[i];
+                
+                generateModal(employee[i])
+
+                let prevEmployee = null;
+                let nextEmployee = null;
+                if (i===0) {
+                    prevEmployee = employee[employeeCards.length-1];
+                    nextEmployee = employee[i+1];
+                } else if (i===employeeCards.length) {
+                    prevEmployee = employee[i-1];
+                    nextEmployee = employee[0];
+                } else {
+                    prevEmployee = employee[i-1];
+                    nextEmployee = employee[i+1];
+                }
+                document.getElementById('modal-next').addEventListener('click', (e) => {
+                    const modalContainer = document.querySelector('.modal-container');
+                    modalContainer.remove();
+                    generateModal(nextEmployee)
+                    listenForModal(data)
+                })
+                document.getElementById('modal-prev').addEventListener('click', (e) => {
+                    const modalContainer = document.querySelector('.modal-container');
+                    modalContainer.remove();
+                    generateModal(prevEmployee)
+                    listenForModal(data)
+                })
+
+            })
+        }
+
+       
 
 }
 
-/**
- * Close Modal
- */
 
 
+
+
+function generateSearch() {
+    const searchContainer = document.querySelector('.search-container');
+    const search = `
+        <form action="#" method="get">
+            <input type="search" id="search-input" class="search-input" placeholder="Search...">
+            <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+        </form>
+        `;
+    searchContainer.insertAdjacentHTML('beforeend', search);
+}
+
+function searchSubmit(data) {
+    generateSearch()
+        const searchForm = document.querySelector('.search-container form');
+        searchForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            for (let i=0; i<gallery.children.length; i++) {
+                gallery.children[i].style.display = 'none';
+            }
+            const nameSearch = document.getElementById('search-input').value.toUpperCase();
+            const employees = data[0].results;
+
+            for (let i=0; i<employees.length;i++) {
+                const employee = employees[i];
+                const firstName = employees[i].name.first;
+                const img = employees[i].picture.large;
+                if (firstName.toUpperCase() === nameSearch) {
+                    gallery.querySelector(`img[src="${img}"]`).parentElement.parentElement.style.display = 'flex';
+                } 
+            }
+        })
+
+        const searchInput = document.querySelector('#search-input');
+        searchInput.addEventListener('change', (e) => {
+            for (let i=0; i<gallery.children.length; i++) {
+                gallery.children[i].style.display = 'flex';
+            }            
+        })
+}
